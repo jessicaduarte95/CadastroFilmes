3600783afa34615fcb654fa6a3808d256b3b2fcc
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-08-14 18:30:37 -0300

    Página Principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,81 @@
+import { Grid } from "@mui/material";
 import React from "react";
-import './App.css';
+import "./App.css";
+import Astronalta from './img/astronalta.png';
+
+const containerStyle = {
+  backgroundImage: 'linear-gradient(90deg, #800080 0%, #C71585 100%)'
+}
+
+const firstPart = {
+  fontFamily: 'Apple Chancery, cursive',
+  color: 'white',
+  fontSize: '70px',
+  paddingLeft: "3.0rem"
+}
+
+const ThirdPart = {
+  fontFamily: 'Apple Chancery, cursive',
+  color: 'white',
+  fontSize: '35px',
+  paddingLeft: "1.5rem",
+  alignContent:'center'
+}
+
+const imgStyle = {
+  maxWidth: '90vh',
+  maxHeight: '98vh',
+  textAlign: 'center'
+}
+
+const buttonStyle = {
+  height: "2.9rem",
+  padding: "0.5rem",
+  borderRadius: "0.4rem",
+  marginTop: "1.5rem",
+  borderColor: "rgb(63, 63, 63)",
+  fontFamily: 'Apple Chancery, cursive',
+  color: 'white',
+  fontSize: '1.2rem',
+  backgroundColor: "rgb(63, 63, 63)"
+}
+
+const Teste = () => {
+  console.log("Testando Botão")
+}
 
 function App() {
   return (
-    <div>
-      <p>Teste</p>
-    </div>
+    <Grid container style={containerStyle}>
+
+      <Grid item xs={4}
+        style={firstPart}
+        display="flex"
+        justifyContent="center"
+        alignItems="center">
+        Bem-vindo ao Bóson!
+      </Grid>
+
+      <Grid item xs={4}
+        display="flex"
+        justifyContent="center"
+        alignItems="center">
+        <img src={Astronalta} style={imgStyle}></img>
+      </Grid>
+
+      <Grid item xs={4}
+        style={ThirdPart}
+        container
+        direction="row"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        >
+        Caso tenha interesse em cadastrar um filme
+        clique no botão abaixo!
+        <button style={buttonStyle} onClick={() => Teste()}>Cadastrar</button>
+      </Grid>
+    </Grid>
   );
 }
 
