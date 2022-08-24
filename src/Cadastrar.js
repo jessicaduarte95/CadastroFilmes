3f964925947407171de007f99b087d5260341aa4
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-08-24 17:21:15 -0300

    Cadastro dos filmes

--- src/Cadastrar.js
+++ src/Cadastrar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, TextareaAutosize } from "@mui/material";
 
 function Cadastrar() {
     const containerStyle = {
@@ -21,13 +21,13 @@ function Cadastrar() {
         backgroundColor: "#C0C0C0",
         maxWidth: "initial",
         display: "flex",
-        height: "75%"
+        height: "100%"
     }
 
     const filmes = {
         margin: "10vh 45vh 10vh 45vh",
         backgroundColor: "white",
-        height: "70%",
+        height: "75%",
         width: "100%",
         borderRadius: "0.4rem",
         display: "flex",
@@ -40,9 +40,26 @@ function Cadastrar() {
         display: "flex",
         alignItems: "center",
         paddingLeft: "0.7rem",
+        marginBottom: "1rem",
         fontFamily: 'Comic Sans MS, Comic Sans, cursive',
         fontSize: '1.8rem',
         color: "#767676",
+    }
+
+    const styleData = {
+        height: "15.5%", 
+        width: "90%",
+        paddingLeft: "0.7rem",
+        fontFamily: 'Comic Sans MS, Comic Sans, cursive',
+        color: "#767676"
+    }
+
+    const styleAnoCategoria = {
+        height: "15.5%", 
+        width: "90%",
+        paddingLeft: "0.7rem",
+        fontFamily: 'Comic Sans MS, Comic Sans, cursive',
+        color: "#767676"
     }
 
     return(
@@ -53,13 +70,28 @@ function Cadastrar() {
             <Grid item sm={12} style={secondPart}>
                 <Grid container item sm={12} style={filmes}>
                     <Typography style={title}>Cadastrar Filme</Typography>
-                    <label style={{height: "50px", width: "100%"}}>
-                        Nome do Filme:
-                        <input type="text" name="name" style={{height: "30px", width: "100%"}}/>
+                    <label style={styleData}>
+                        Título
+                        <input type="text" name="name" style={{height: "30px", width: "100%", marginTop: "0.3rem"}}/>
                     </label>
-                    <label style={{height: "65px", width: "100%"}}>
-                        Descrição:
-                        <input type="text" name="name" style={{height: "30px", width: "100%"}}/>
+                    <label style={styleAnoCategoria}>
+                        Ano
+                        <input type="text" name="name" style={{height: "30px", width: "100%", marginTop: "0.3rem"}}/>
+                    </label>
+                    <label style={styleAnoCategoria}>
+                        Categoria
+                        <input type="text" name="name" style={{height: "30px", width: "100%", marginTop: "0.3rem"}}/>
+                    </label>
+                    <label style={styleData}>
+                        Sinopse
+                        <TextareaAutosize
+                        maxRows={4}
+                        minRows={4}
+                        aria-label="maximum height"
+                        placeholder="Descrição do Filme"
+                        defaultValue=""
+                        style={{ width: 700, marginTop: "0.3rem" }}
+                        />
                     </label>
                 </Grid>
             </Grid>
