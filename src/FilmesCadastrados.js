b3414c33a22c32266a764ea020a0c4168c6cf14a
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-09-01 19:44:48 -0300

    Filmes Cadastrados

--- src/FilmesCadastrados.js
+++ src/FilmesCadastrados.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 function FilmesCadastrados() {
 
     const containerStyle = {
-        flexDirection: "column"
+        flexDirection: "row"
     }
 
     const firstPart = {
         backgroundImage: "linear-gradient(90deg, #800080 0%, #C71585 100%)",
-        width: "100%",
-        maxWidth: "initial",
+        height: "25%",
         padding: "0vh 45vh 2vh 45vh",
         display: "flex",
         alignItems:"flex-end",
@@ -22,25 +21,46 @@ function FilmesCadastrados() {
     const secondPart = {
         backgroundColor: "#C0C0C0",
         maxWidth: "initial",
-        display: "flex"
+        display: "flex",
+        paddingTop: "6vh",
+        flexDirection: "column"
     }
 
     const filmes = {
-        margin: "10vh 45vh 10vh 45vh",
+        margin: "0vh 45vh 8vh 45vh",
         backgroundColor: "white",
-        height: "70%",
-        width: "100%",
-        borderRadius: "0.4rem"
+        height: "35%",
+        width: "60%",
+        borderRadius: "0.4rem",
+        direction: "column",
+        flex: "wrap"
+    }
+
+    const title = {
+        height: "13%",
+        display: "flex",
+        alignItems: "center",
+        paddingLeft: "1.5rem",
+        marginBottom: "0.3rem",
+        fontFamily: 'Comic Sans MS, Comic Sans, cursive',
+        fontSize: '1rem',
+        color: "#474747",
     }
 
     return(
         <Grid container style={containerStyle}>
-            <Grid item xs={3} style={firstPart}>
+            <Grid item sm={12} style={firstPart}>
                 Bóson
             </Grid>
-            <Grid item xs={9} style={secondPart}>
-                <Grid item style={filmes}>
-                    
+            <Grid item sm={12} style={secondPart}>
+                <Grid item sm={12} style={filmes}>
+                    <Typography style={title}>Título do Filme</Typography>
+                    <Typography style={title}>Ano de Estreia do filme</Typography>
+                    <Typography style={title}>Categoria do Filme</Typography>
+                    <Typography style={title}>Sinopse</Typography>
+                </Grid>
+                <Grid container item sm={12} style={filmes}>
+                    Teste2
                 </Grid>
             </Grid>
         </Grid>
