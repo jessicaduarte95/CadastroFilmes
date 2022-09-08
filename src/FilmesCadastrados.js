b6a7860e2baef8d2bd6e3623f434b8cb28d83fe1
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-09-08 20:01:01 -0300

    Modal Excluir

--- src/FilmesCadastrados.js
+++ src/FilmesCadastrados.js
@@ -6,6 +6,10 @@ import { useState } from "react";
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import CreateIcon from '@mui/icons-material/Create';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Backdrop from '@mui/material/Backdrop';
 
 function FilmesCadastrados() {
 
@@ -91,7 +95,23 @@ function FilmesCadastrados() {
         color: "#3E3E3E"
     }
 
+    const styleModalExcluir = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+        height: 150
+      };
+
     const [dataMovies, setDataMovies] = useState();
+    const [openExcluir, setOpenExcluir] = useState(false);
+    const handleOpenExcluir = () => setOpenExcluir(true);
+    const handleCloseExcluir = () => setOpenExcluir(false);   
 
     useEffect(() => {
         Axios.get("http://localhost:5000/filmesCadastrados")
@@ -117,7 +137,7 @@ function FilmesCadastrados() {
                         <Grid item style={{height: "20px"}}>
                             <PictureAsPdfIcon style={{height: "20px"}}/>
                             <CreateIcon style={{height: "20px"}}/>
-                            <DeleteOutlinedIcon style={{height: "20px"}}/>
+                            <DeleteOutlinedIcon style={{height: "20px"}} onClick={handleOpenExcluir}/>
                         </Grid>
                     </Grid>
                     <Typography name="year" style={text}>{item.year}</Typography>
@@ -126,7 +146,30 @@ function FilmesCadastrados() {
                 </Grid>
                 )}
             </Grid>
+            <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={openExcluir}
+            onClose={handleCloseExcluir}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+            timeout: 500,
+            }}
+            >
+                <Fade in={openExcluir}>
+                    <Box sx={styleModalExcluir}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2" style={{height: "20px"}}>
+                        Text in a modal
+                        </Typography>
+                        <Typography id="transition-modal-description" sx={{ mt: 2 }} style={{height: "30px"}}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Fade>
+            </Modal>
         </Grid>
+        
     )
 }
 
